78911e4e555d3bfbada0fd72d88346f73883e4ba
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-08-16 22:04:07 -0400

    introduccion as tipos de datos

---
+++ basico/IntroducciónTiposDatos.js
@@ -0,0 +1,39 @@
+
+//! Introducción a los tipos de datos
+
+//? Primitivos
+//*   cadena  
+//*   numero  
+//*   booleando  
+//*   indefinido  
+//*   nulo  
+//*   simbolo 
+
+//? Numeros
+//* Numeros enteros : (negativos , cero y positivos) Ejemplo : ----3,-2,-1,0,1,2,3...
+//* Numeros de punto flotante : numero decimales Ejemplos : -3.5, -2.25,-1.0, 0.0,1.1,2.2,....
+//? Cadenas
+//* Una colecion de uno o más caracteres entre dos comillas simples, comillas dobles o acentos graves.
+'a'
+'maria'
+"maria"
+`maria`
+//? Booleanos 
+
+//* Un valor de verdader o falso , cualquier comparacion devuelve un valor booleano, que es verdadero o falso
+//* Ejemplos
+    true, false
+//? Indefinido
+let x
+console.log(x);
+//? Null , nulo
+let y=null
+console.log(y);
+
+//? Comprobacion de tipos de datos
+console.log(typeof 'maria');
+console.log(typeof 3); 
+console.log(typeof true); 
+
+
+
