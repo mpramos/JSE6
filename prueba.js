eb6763449c65e3d2dc76629046bf9a8fbf2f68df
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-08-19 11:12:40 -0400

    introduccion

--- prueba.js
+++ prueba.js
@@ -1,7 +1,15 @@
 console.log('hola');
+debugger
+console.log(numero);
+debugger
+var numero=2
+debugger
+console.log(numero);
+debugger
 console.log('hola');
-console.log('hola');
+console.log('hola mundo','hola a todos', 2+4,{suma:2+4},['maria','carla']);
 console.log(3+5);
 console.log(3-5);
 console.log(3*5);
 console.log(3/5);
+
